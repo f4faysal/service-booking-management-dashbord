1f54219cb59632adf08d77775c09ac5c4424109c
f4faysal <f4faysals@gmail.com>
2023-10-16 08:55:38 +0600

    Faq done

--- src/app/(withlayout)/admin/faqs/edit/[id]/page.tsx
+++ src/app/(withlayout)/admin/faqs/edit/[id]/page.tsx
@@ -4,45 +4,30 @@ import Loading from "@/app/loading";
 import FormTextArea from "@/components/forms/FormTextArea.tsx";
 import Form from "@/components/forms/form";
 import FormInput from "@/components/forms/formInput";
-import FormSelectField from "@/components/forms/formSelectField";
-import ServiceCategoreField from "@/components/forms/service-catagory";
 import SMBreadcrumb from "@/components/ui/Breadcrumb";
-import ImageUpload from "@/components/ui/image-upload";
-import { locationOption } from "@/constants/golobal";
-import {
-  useServicesQuery,
-  useUpdateServicesMutation,
-} from "@/redux/api/serviceApi";
+import { useFaqQuery, useUpdateFaqMutation } from "@/redux/api/faqsApi";
 import { getUserInfo } from "@/services/auth.service";
-import { Avatar, Button, Col, Row, message } from "antd";
-import { useState } from "react";
+import { Button, Col, Row, message } from "antd";
 
-const EditServicePage = ({ params }: any) => {
+const EditFaqsPage = ({ params }: any) => {
   const id = params.id;
 
   const { role } = getUserInfo() as any;
-  const [imageUrl, setImageUrl] = useState(
-    "https://res.cloudinary.com/dhvuyehnq/image/upload/v1697354272/gcu3mnulmato2odnqqvp.png"
-  );
 
-  const { data, isLoading, refetch } = useServicesQuery(id);
+  const { data, isLoading, refetch } = useFaqQuery(id);
 
-  const service = data?.data;
+  const faq = data?.data;
 
-  const [updateServices] = useUpdateServicesMutation();
+  const [updateFaq] = useUpdateFaqMutation();
 
   const onSubmit = async (data: any) => {
-    message.loading("Adding Service...");
+    message.loading("Updating Blog...");
     try {
-      const serviceData = { imageLink: imageUrl, ...data };
-      console.log(serviceData);
-      const res = await updateServices({ body: serviceData, id }).unwrap();
+      console.log(data);
+      const res = await updateFaq({ body: data, id }).unwrap();
       console.log(res);
       if (res?.success) {
-        setImageUrl(
-          "https://res.cloudinary.com/dhvuyehnq/image/upload/v1697354272/gcu3mnulmato2odnqqvp.png"
-        );
-        message.success("Service added successfully");
+        message.success("Blog Update successfully");
         refetch();
       }
     } catch (err: any) {
@@ -51,12 +36,8 @@ const EditServicePage = ({ params }: any) => {
   };
 
   const defaultValues = {
-    title: service?.title,
-    price: service?.price,
-    tax: service?.tax,
-    location: service?.location,
-    description: service?.location,
-    imageLink: service?.imageLink,
+    question: faq?.question,
+    answer: faq?.answer,
   };
   if (isLoading) <Loading />;
 
@@ -65,63 +46,25 @@ const EditServicePage = ({ params }: any) => {
       <SMBreadcrumb
         items={[
           {
-            label: "Manage Services",
-            path: `/${role}/services`,
+            label: "Manage Blog",
+            path: `/${role}/blogs`,
           },
           {
-            label: "Create services",
+            label: "Update Blog",
           },
         ]}
       />
 
-      <h1>Create Services</h1>
+      <h1>Update Faq</h1>
       <Form submitHandler={onSubmit} defaultValues={defaultValues}>
         <Row gutter={{ xs: 24, xl: 8, lg: 8, md: 24 }}>
           <Col span={8} style={{ margin: "10px 0" }}>
             <FormInput
-              name="title"
-              label="Service Title"
+              name="question"
+              label="Faqs Question"
               type="text"
-              placeholder="Service Title"
+              placeholder="Faqs Question"
               size="large"
-            />
-          </Col>
-        </Row>
-
-        <Row gutter={{ xs: 24, xl: 8, lg: 8, md: 24 }}>
-          <Col span={8} style={{ margin: "10px 0" }}>
-            <FormInput
-              name="price"
-              label="Service Price"
-              type="text"
-              placeholder="Service Price"
-              size="large"
-            />
-          </Col>
-        </Row>
-        <Row gutter={{ xs: 24, xl: 8, lg: 8, md: 24 }}>
-          <Col span={8} style={{ margin: "10px 0" }}>
-            <FormInput
-              name="tax"
-              label="Service Tax %"
-              type="text"
-              placeholder="Service Tax = price - (price * tax / 100)"
-              size="large"
-            />
-          </Col>
-        </Row>
-        <Row gutter={{ xs: 24, xl: 8, lg: 8, md: 24 }}>
-          <Col span={8} style={{ margin: "10px 0" }}>
-            <ServiceCategoreField label="Service Category" name="categoryId" />
-          </Col>
-        </Row>
-        <Row gutter={{ xs: 24, xl: 8, lg: 8, md: 24 }}>
-          <Col span={8} style={{ margin: "10px 0" }}>
-            <FormSelectField
-              name="location"
-              label="Service Location"
-              options={locationOption}
-              placeholder="Select location"
             />
           </Col>
         </Row>
@@ -129,36 +72,19 @@ const EditServicePage = ({ params }: any) => {
         <Row gutter={{ xs: 24, xl: 8, lg: 8, md: 24 }}>
           <Col span={8} style={{ margin: "10px 0" }}>
             <FormTextArea
-              name="description"
-              label="Service Description"
-              placeholder="Service Description"
+              name="answer"
+              label="Faqs Answer"
+              placeholder="Faqs Answer"
             />
           </Col>
         </Row>
 
-        <Row gutter={{ xs: 24, xl: 8, lg: 8, md: 24 }}>
-          <Col
-            span={8}
-            style={{
-              margin: "10px 0",
-              display: "flex",
-              flexDirection: "column",
-              justifyContent: "center",
-              alignItems: "start",
-              gap: "10px",
-            }}
-          >
-            <Avatar shape="square" size={200} src={imageUrl} />
-            <ImageUpload setImageUrl={setImageUrl} imageUrl={imageUrl} />
-          </Col>
-        </Row>
-
         <Button type="primary" htmlType="submit">
-          Add
+          Update
         </Button>
       </Form>
     </div>
   );
 };
 
-export default EditServicePage;
+export default EditFaqsPage;
